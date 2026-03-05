33bcdc37eea03906927354e7d7d60f26bf6b6ef8
Oleh Hanhal <hanhaloleg59@gmail.com>
2026-03-05 14:02:16 +0200

    add Tokenize, TurnToRPN, create CustomStack, CustomArrayList classes

---
+++ CS210_Assignment1_Oleh_Hanhal/Calculator.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+
+namespace CS210_Assignment1_Oleh_Hanhal;
+
+public class Calculator
+{
+    private CustomArrayList _tokens = new CustomArrayList();
+    
+    public void Tokenize(string input)
+    {
+        string buffer = "";
+
+        foreach (var s in input)
+        {
+            if (char.IsDigit(s))
+            {
+                buffer += s.ToString();
+            }
+            
+            else if (s == '+' || s == '-' || s == '*' || s == '/' || s == '(' || s == ')')
+            {
+                if (buffer != "")
+                {
+                    _tokens.Add(buffer);
+                    buffer = "";
+                }
+                _tokens.Add(s.ToString());
+            }
+            
+            else if (s == ' ')
+            {
+                if (buffer != "")
+                {
+                    _tokens.Add(buffer);
+                    buffer = "";
+                }
+            }
+        }
+        
+        if (buffer != "")
+        {
+            _tokens.Add(buffer);
+        }
+    }    
+    
+    public string TurnToRPN()
+    {
+        Dictionary<string, int> priorities = new Dictionary<string, int>
+        {
+            { "(", 0 },
+            { "+", 1 },
+            { "-", 1 },
+            { "*", 2 },
+            { "/", 2 }
+        };
+
+        CustomStack operatorStack = new CustomStack();
+        string result = "";
+
+        for (var i = 0; i < _tokens.Count(); i++)
+        {
+            string token = _tokens.GetAt(i);
+            if (int.TryParse(token, out int parsed_number))
+            {
+                result += token + " ";
+            }
+
+            else if (token == "+" || token == "-" || token == "*" || token == "/")
+            {
+                while (operatorStack.Peek() != null &&
+                       operatorStack.Peek() != "(" &&
+                       priorities[token] <= priorities[operatorStack.Peek()])
+                {
+                    result += operatorStack.Pop() + " ";
+                }
+
+                operatorStack.Push(token);
+            }
+
+            else if (token == "(")
+            {
+                operatorStack.Push(token);
+            }
+
+            else if (token == ")")
+            {
+                while (operatorStack.Peek() != null &&
+                       operatorStack.Peek() != "(")
+                {
+                        result += operatorStack.Pop() + " ";
+                }
+
+                operatorStack.Pop();
+            }
+        }
+
+        while (operatorStack.Peek() != null)
+        {
+            result += operatorStack.Pop() + " ";
+        }
+
+        return result.Trim();
+    }
+}
---
+++ CS210_Assignment1_Oleh_Hanhal/Stack.cs
@@ -0,0 +1,50 @@
+﻿namespace CS210_Assignment1_Oleh_Hanhal;
+
+public class CustomStack
+{
+    private const int Capacity = 50;
+
+    private string[] _array = new string[Capacity];
+
+    private int _pointer;
+
+    public void Push(string value)
+    {
+        if (_pointer == _array.Length)
+        {
+            throw new Exception("Stack overflowed");
+        }
+
+        _array[_pointer] = value;
+        _pointer++;
+    }
+
+    public string Pop()
+    {
+        if (_pointer == 0)
+        {
+            return null;
+        }
+        
+        _pointer--;
+        var value = _array[_pointer];
+        return value;
+    }
+
+    public string Peek()
+    {
+        if (_pointer > 0)
+        {
+            var value = _array[_pointer - 1];   
+            
+            return value;
+        }
+        
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        return null;
+    }
+}
